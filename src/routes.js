5f1e413159bcf9d30f9fd01b7ec9045812308595
Wilson Zeng <jackzcs@gmail.com>
2014-12-20 14:47:09 +0800

    add an option: selector

--- src/routes.js
+++ src/routes.js
@@ -33,7 +33,8 @@ function createSchema() {
         images: joi.boolean(),
         user: joi.string().trim(),
         password: joi.string().trim(),
-        callback: joi.string().trim()
+        callback: joi.string().trim(),
+		selector: joi.string().trim() //zcs=Support selector under phantomjs
     });
 }
 
